fdbdf4c1101c402f2341b3b08a9fb30f79395142
alimaqsood19 <ali-maqsood@Hotmail.com>
2017-06-22 22:25:41 -0400

    Started react-redux application with boiler plates

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  return () {
+    return (
+      <div>Hello!</div>
+    );
+  }
+}
+
+class Goodbye extends React.Component {
+  return () {
+    return (
+      <div>Goodbye!</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
